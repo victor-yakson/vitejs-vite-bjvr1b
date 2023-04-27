44d1cc1d36f973c58e8943b1b5a4ab90f05bc62b
victor-yakson <84426174+victor-yakson@users.noreply.github.com>
2023-04-27 08:31:43 +0100

    3 files modified, 2 added, 1 deleted

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { configureChains, createClient } from "@wagmi/core";
+import { arbitrum, avalanche, mainnet, polygon } from "@wagmi/core/chains";
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/html";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// 1. Define constants
+const projectId = import.meta.env.VITE_PROJECT_ID;
+const chains = [mainnet, polygon, avalanche, arbitrum];
 
-setupCounter(document.querySelector('#counter'))
+// 2. Configure wagmi client
+const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: w3mConnectors({ chains, version: 1, projectId }),
+  provider,
+});
+
+// 3. Create ethereum and modal clients
+const ethereumClient = new EthereumClient(wagmiClient, chains);
+export const web3Modal = new Web3Modal(
+  {
+    projectId,
+    walletImages: {
+      safe: "https://pbs.twimg.com/profile_images/1566773491764023297/IvmCdGnM_400x400.jpg",
+    },
+  },
+  ethereumClient
+);
